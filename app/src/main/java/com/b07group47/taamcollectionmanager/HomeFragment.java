da5ebf3d5e837b4be55063d9fe387d841ed66963
Mark Onopchenko <mark.onopchenko@mail.utoronto.ca>
2024-07-17 21:20:00 -0400

    Major UI Redisign
    Redesigns the Home screen to be more in line with the project handout.

--- app/src/main/java/com/b07group47/taamcollectionmanager/HomeFragment.java
+++ app/src/main/java/com/b07group47/taamcollectionmanager/HomeFragment.java
@@ -10,18 +10,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.b07group47.taamcollectionmanager.R;
-
 public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_home_fragment, container, false);
 
-        Button buttonRecyclerView = view.findViewById(R.id.buttonRecyclerView);
-        Button buttonScroller = view.findViewById(R.id.buttonScroller);
-        Button buttonSpinner = view.findViewById(R.id.buttonSpinner);
-        Button buttonManageItems = view.findViewById(R.id.buttonManageItems);
+//        Button buttonRecyclerView = view.findViewById(R.id.buttonRecyclerView);
+//        Button buttonScroller = view.findViewById(R.id.buttonScroller);
+//        Button buttonSpinner = view.findViewById(R.id.buttonSpinner);
+        Button buttonManageItems = view.findViewById(R.id.buttonView);
         Button buttonSearch = view.findViewById(R.id.buttonSearch);
         Button buttonAdmin = view.findViewById(R.id.buttonAdmin);
         Button buttonReport = view.findViewById(R.id.buttonReport);
@@ -33,30 +31,23 @@ public class HomeFragment extends Fragment {
                 loadFragment(new MainTableFragment());}
         });
 
-        buttonRecyclerView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadFragment(new RecyclerViewFragment());
-            }
-        });
+//        buttonScroller.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                loadFragment(new ScrollerFragment());
+//            }
+//        });
 
-        buttonScroller.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadFragment(new ScrollerFragment());
-            }
-        });
-
-        buttonSpinner.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadFragment(new SpinnerFragment());
-            }
-        });
+//        buttonSpinner.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                loadFragment(new SpinnerFragment());
+//            }
+//        });
 
         buttonManageItems.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { loadFragment(new ManageItemsFragment());}
+            public void onClick(View v) { loadFragment(new ViewFragment());}
         });
 
         buttonSearch.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/b07group47/taamcollectionmanager/MainActivity.java
+++ app/src/main/java/com/b07group47/taamcollectionmanager/MainActivity.java
@@ -6,8 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.b07group47.taamcollectionmanager.R;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.base_layout);
 
         //db = FirebaseDatabase.getInstance("https://b07-demo-summer-2024-default-rtdb.firebaseio.com/");
         //DatabaseReference myRef = db.getReference("testDemo");
